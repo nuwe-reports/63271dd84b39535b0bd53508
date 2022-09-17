8f78fe06300f2dd36632e07fe5f40dc80e527a22
eperez045 <eperez045@gmail.com>
2022-09-17 18:12:44 +0200

    inyeccion dependencias

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,25 +1,71 @@
-import { Body, Controller, Post, Get, Put, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Res,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
+import { response } from 'express';
 import { CreateJobDto } from './dto/create-job-dto';
+import { JobsService } from './jobs.service';
 
 @Controller('jobs')
 export class JobsController {
+  constructor(private jobsService: JobsService) {}
+
   @Post()
-  create(@Body() createJobDto: CreateJobDto) {
-    return 'job create';
+  create(@Body() createJobDto: CreateJobDto, @Res() response) {
+    this.jobsService
+      .createJob(createJobDto)
+      .then((job) => {
+        response.status(HttpStatus.CREATED).json(job);
+      })
+      .catch(() => {
+        response.status(HttpStatus.FORBIDDEN).json({ mensaje: 'error' });
+      });
   }
 
   @Get()
-  getAll() {
-    return 'job list';
+  getAll(@Res() response) {
+    this.jobsService
+      .getAll()
+      .then((jobsList) => {
+        response.status(HttpStatus.OK).json(jobsList);
+      })
+      .catch(() => {
+        response.status(HttpStatus.FORBIDDEN).json({ mensaje: 'error' });
+      });
   }
 
-  @Put('::id')
-  update(@Body() updateJobDto: CreateJobDto) {
-    return 'job updated';
+  @Put(':id')
+  update(
+    @Body() updateJobDto: CreateJobDto,
+    @Res() response,
+    @Param('id') jobId,
+  ) {
+    this.jobsService
+      .updateJob(jobId, updateJobDto)
+      .then((job) => {
+        response.status(HttpStatus.OK).json(job);
+      })
+      .catch(() => {
+        response.status(HttpStatus.FORBIDDEN).json({ mensaje: 'error' });
+      });
   }
 
-  @Delete('::id')
-  delete() {
-    return 'job deleted';
+  @Delete(':id')
+  delete(@Res() response, @Param('id') jobId) {
+    this.jobsService
+      .deleteJob(jobId)
+      .then((res) => {
+        response.status(HttpStatus.OK).json(res);
+      })
+      .catch(() => {
+        response.status(HttpStatus.FORBIDDEN).json({ mensaje: 'error' });
+      });
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -11,17 +11,32 @@ export class JobsService {
     private jobRepository: Repository<Job>,
   ) {}
 
-  async getAll() {
+  async getAll(): Promise<Job[]> {
     return await this.jobRepository.find();
   }
 
-  async createJob(newJob: CreateJobDto) {
+  async createJob(newJob: CreateJobDto): Promise<Job> {
     const nuevo = new Job();
     nuevo.title = newJob.title;
     nuevo.description = newJob.description;
     nuevo.company = newJob.company;
     nuevo.localization = newJob.localization;
     nuevo.salary = newJob.salary;
-    return await this.jobRepository
+    return this.jobRepository.save(nuevo);
+  }
+
+  async updateJob(idJob: number, actualizeJob: CreateJobDto): Promise<Job> {
+    const jobUpdate = await this.jobRepository.findOne(idJob);
+    jobUpdate.title = actualizeJob.title;
+    jobUpdate.description = actualizeJob.description;
+    jobUpdate.company = actualizeJob.company;
+    jobUpdate.localization = actualizeJob.localization;
+    jobUpdate.salary = actualizeJob.salary;
+
+    return await this.jobRepository.save(jobUpdate);
+  }
+
+  async deleteJob(idJob: number): Promise<any> {
+    return await this.jobRepository.delete(idJob);
   }
 }
